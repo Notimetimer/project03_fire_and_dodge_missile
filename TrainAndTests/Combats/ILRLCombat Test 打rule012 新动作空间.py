3b08789acffc8bd7973a4d3fb196557ba4e8ee8e
Yuhui Liu <2284846497@qq.com>
2025-12-28 15:13:16 +0800

    Update ILRLCombat Test 打rule012 新动作空间.py

--- TrainAndTests/Combats/ILRLCombat Test 打rule012 新动作空间.py
+++ TrainAndTests/Combats/ILRLCombat Test 打rule012 新动作空间.py
@@ -45,14 +45,15 @@ def create_initial_state():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("RL/IL Combat Test")
     parser.add_argument("--agent-id", type=int, default=None, help="Specific agent ID to test. If None, loads the latest.")
-    parser.add_argument("--mission-name", type=str, default='RL_combat_PFSP_简单熵_区分左右_无淘汰机制', help="Mission name to find the log directory.")
+    parser.add_argument("--mission-name", type=str, default='RL_combat_PFSP_简单熵_区分左右_无淘汰机制_开火负熵', help="Mission name to find the log directory.")
     args = parser.parse_args()
 
     'RL_combat_PFSP_简单熵_区分左右'
     'IL_RL_combat_PFSP_简单熵_区分左右'
-    'RL_combat_PFSP_简单熵_区分左右_无淘汰机制'
     '打莽夫_左右_noCA_4epochs'
     '打莽夫_左右_CA_4epochs'
+    'RL_combat_PFSP_简单熵_区分左右_无淘汰机制'
+    'RL_combat_PFSP_简单熵_区分左右_无淘汰机制_开火负熵'
     
     
     # --- 环境和模型参数 (必须与训练时一致) ---
