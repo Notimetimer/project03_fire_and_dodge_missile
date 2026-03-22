ea4d7fb361328fb8be4bc70d6d01b9fb56fe4623
Yuhui Liu <2284846497@qq.com>
2026-03-22 15:23:36 +0800

    信号无关高度，就不要有高度误差了

--- TrainAndTests/Controls/绘制误差曲线 无对比.py
+++ TrainAndTests/Controls/绘制误差曲线 无对比.py
@@ -16,7 +16,7 @@ current_dir = os.path.join(project_root, "TrainAndTests/Controls")
 test_res_dir = os.path.join(current_dir, 'test_result')
 
 file_name = "FlightControl_parallel目标会动_高度可超调_有过载限制_动态lr_wave_.csv"
-file_name2 = "PID_wave_.csv"
+# file_name2 = "PID_wave_.csv"
 
 def load_processed_data(file_path):
     if not os.path.exists(file_path):
@@ -29,7 +29,7 @@ def load_processed_data(file_path):
 
 # 加载两个文件的数据
 df1 = load_processed_data(os.path.join(test_res_dir, file_name))
-df2 = load_processed_data(os.path.join(test_res_dir, file_name2))
+# df2 = load_processed_data(os.path.join(test_res_dir, file_name2))
 
 # 创建画布
 plt.figure(figsize=(15, 10))
@@ -55,32 +55,32 @@ def plot_comparison(ax, df_list, col_name, labels, is_error=False, req_col=None)
 
 # 1. 航向角误差对比
 ax1 = plt.subplot(3, 1, 1)
-plot_comparison(ax1, [df1, df2], 'psi', ['RL', 'PID'], is_error=True, req_col='psi_req')
+plot_comparison(ax1, [df1], 'psi', ['RL', 'PID'], is_error=True, req_col='psi_req')
 ax1.set_title("航向角误差 (Heading Error) 对比")
 ax1.set_ylabel(r"$\varepsilon_{\psi}$ (°)")
 ax1.legend(); ax1.grid(True)
 
 # 2. 俯仰角对比
 ax2 = plt.subplot(3, 1, 2)
-plot_comparison(ax2, [df1, df2], 'theta', ['RL', 'PID'], req_col='theta_req')
+plot_comparison(ax2, [df1], 'theta', ['RL', 'PID'], req_col='theta_req')
 ax2.set_title("俯仰角 (Pitch) 跟踪对比")
 ax2.set_ylabel(r"$\theta$ (°)")
 ax2.legend(); ax2.grid(True)
 
 # 3. 速度对比
 ax3 = plt.subplot(3, 1, 3)
-plot_comparison(ax3, [df1, df2], 'v', ['RL', 'PID'], req_col='v_req')
+plot_comparison(ax3, [df1], 'v', ['RL', 'PID'], req_col='v_req')
 ax3.set_title("速度 (Velocity) 跟踪对比")
 ax3.set_ylabel("v (m/s)")
 ax3.legend(); ax3.grid(True)
 
 # 4. 高度对比
 # ax4 = plt.subplot(2, 2, 4)
-# plot_comparison(ax4, [df1, df2], 'h', ['RL', 'PID'], req_col='h_req')
+# plot_comparison(ax4, [df1], 'h', ['RL', 'PID'], req_col='h_req')
 # ax4.set_title("高度 (Altitude) 跟踪对比")
 # ax4.set_ylabel("Height (m)")
 # ax4.legend(); ax4.grid(True)
 
-plt.suptitle(f"控制器性能对比\nRL: {file_name}\nPID: {file_name2}", fontsize=12)
+plt.suptitle(f"控制器性能\n: {file_name}", fontsize=12)
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 plt.show()
