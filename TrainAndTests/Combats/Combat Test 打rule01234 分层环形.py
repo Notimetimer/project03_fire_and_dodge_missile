5e4f23771f136a6a3a9092300ab59e26d1ba80af
Yuhui Liu <2284846497@qq.com>
2026-05-14 16:48:47 +0800

    Update Combat Test 打rule01234 分层环形.py

--- TrainAndTests/Combats/Combat Test 打rule01234 分层环形.py
+++ TrainAndTests/Combats/Combat Test 打rule01234 分层环形.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
                         # 超控标准差，检查熵
                         actor_wrapper.net.log_std_shared.fill_(np.log(0.2)) # 0.8
                         r_action_exec, _, _, r_action_check = actor_wrapper.get_action(
-                            r_obs, explore={'lin':1, 'circ':1, 'bern':1}, check_obs=None, bern_threshold=0.4
+                            r_obs, explore={'lin':0, 'circ':0, 'bern':1}, check_obs=None, bern_threshold=0.4
                             ) # check_obs=r_check_obs, check_obs=None
                         
                     r_action_v = r_action_exec['lin'][0] if isinstance(r_action_exec['lin'], (list, np.ndarray)) else r_action_exec['lin']
