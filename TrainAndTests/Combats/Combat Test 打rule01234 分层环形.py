6826007cfd9ef8912aab94ff7c310608effc87f0
Yuhui Liu <2284846497@qq.com>
2026-05-20 23:27:24 +0800

    1

--- TrainAndTests/Combats/Combat Test 打rule01234 分层环形.py
+++ TrainAndTests/Combats/Combat Test 打rule01234 分层环形.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     args.agent_id = None # 172 # 838
     
     # --- 环境和模型参数 (必须与训练时一致) ---
-    env_args = argparse.Namespace(max_episode_len=15*60, R_cage=55.00e3) # 55e3
+    env_args = argparse.Namespace(max_episode_len=15*60, R_cage=62.00e3) # 55e3
     hidden_dim = [128, 128, 128]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
--- TrainAndTests/Combats/Combat Test 自博弈测试 分层.py
+++ TrainAndTests/Combats/Combat Test 自博弈测试 分层.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     blue_agent_id = None # 200
     
     # --- 环境和模型参数 (必须与训练时一致) ---
-    env_args = argparse.Namespace(max_episode_len=15*60, R_cage=55.00e3) # 55e3
+    env_args = argparse.Namespace(max_episode_len=15*60, R_cage=62.00e3) # 55e3
     hidden_dim = [128, 128, 128]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
