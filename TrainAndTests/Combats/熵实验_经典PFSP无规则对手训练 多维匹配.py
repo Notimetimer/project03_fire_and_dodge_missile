fcc3f1755ebeb21a8a4e9e91ebd5a5c457b0490c
Yuhui Liu <2284846497@qq.com>
2026-05-25 21:54:22 +0800

    1

--- TrainAndTests/Combats/熵实验_经典PFSP无规则对手训练 多维匹配.py
+++ TrainAndTests/Combats/熵实验_经典PFSP无规则对手训练 多维匹配.py
@@ -4,7 +4,7 @@ from prepare_il_datas_hierarchical import run_rules
 
 # 指定中断续训的目录。如果为 None，则正常开启新训练。
 resume_target_dir = None
-# resume_target_dir = r"D:\3_Machine_Learning_in_Python\project03_fire_and_dodge_missile\logs\combat\IL_and_Pure经典PFSP_挑战_并行_分层_rule3_0.3-run-20260520-233351"
+resume_target_dir = r"D:\3_Machine_Learning_in_Python\project03_fire_and_dodge_missile\logs\combat\IL_and_Pure经典PFSP_多技术流派_并行_分层_rule3_0.3_3类-run-20260524-174414"
 collape_recover={ # 是否是崩盘后恢复 
             "collapsed": False,
             "best_actor_name": None,
