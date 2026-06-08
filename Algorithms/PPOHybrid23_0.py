e2b4e72b9574e6050e8f70722c79f94f49fa93bf
Yuhui Liu <2284846497@qq.com>
2026-06-08 09:44:24 +0800

    Update PPOHybrid23_0.py

--- Algorithms/PPOHybrid23_0.py
+++ Algorithms/PPOHybrid23_0.py
@@ -127,7 +127,7 @@ class PolicyNetHybrid(torch.nn.Module):
             保险起见折个半，在18s内取憋着不开火的概率为p18=0.5，这样就是同时满足开火概率p18=(1-p0)^9=0.5,
             (1-1/(1+e^x))^9=0.5, 解出bern_logits=-2.5
             """
-            nn.init.constant_(self.fc_bern[-1].bias, 0) # -2.5) # 2.0
+            nn.init.constant_(self.fc_bern[-1].bias, -2.0) # -2.5) # 2.0
             
             # 为每一个伯努利动作维度创建一个温度参数
             # 初始化为 0 (即 temperature=1.0)
