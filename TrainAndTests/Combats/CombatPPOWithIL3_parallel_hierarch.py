8785e5ca751423e5b1563c64b6b5e2953d40cf81
Yuhui Liu <2284846497@qq.com>
2026-09-08 10:32:04 +0800

    Update CombatPPOWithIL3_parallel_hierarch.py

--- TrainAndTests/Combats/CombatPPOWithIL3_parallel_hierarch.py
+++ TrainAndTests/Combats/CombatPPOWithIL3_parallel_hierarch.py
@@ -1730,7 +1730,7 @@ def run_MLP_simulation(
                     'blue_birth': bb,
                     # 'R_cage_range': R_cage_range, # 将范围传给Worker
                     'fire_mask': fire_mask,
-                    'end_reward_weight': np.clip(total_steps/18e3, 0.3, 0.5),
+                    'end_reward_weight': 0.3 + np.clip(total_steps/18e3, 0, 1)*(0.4-0.3),
                     'fire_inside_weight': fire_inside_weight,
                     'fire_reward_weight': fire_reward_weight,
                 }
